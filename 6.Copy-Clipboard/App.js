cadf20a55600ae72bb2f6e6c128260f9e22f7aaf
enginecode-cpu <bassmatics1002@gmail.com>
2020-11-03 02:04:38 +0900

    Copy to Clipboard

---
+++ 6.Copy-Clipboard/App.js
@@ -0,0 +1,25 @@
+const text = document.querySelector('.content h2');
+const popup = document.querySelector('.popup');
+
+text.addEventListener('click', () => {
+  popup.classList.add('active');
+  copyToClipboard();
+});
+
+popup.addEventListener('animationend', () => {
+  popup.classList.remove('active');
+});
+
+function copyToClipboard() {
+  const textArea = document.createElement('textarea');
+  textArea.setAttribute('readonly', '');
+  textArea.style.position = 'absolute';
+  textArea.value = text.innerText;
+
+  document.body.appendChild(textArea);
+  textArea.select();
+
+  document.execCommand('copy');
+
+  document.body.removeChild(textArea);
+}
